fbf3d4d46f1695591741a91dace9984df1daca17
emirmusul <musulemir@gmail.com>
2026-07-05 12:50:02 +0300

    feat: hello world via UART

---
+++ main/esp32-uart.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+void app_main(void)
+{
+    while(true)
+    {
+        printf("Hello from ESP32!\n");
+        vTaskDelay(pdMS_TO_TICKS(1000));
+    }
+
+
+}
